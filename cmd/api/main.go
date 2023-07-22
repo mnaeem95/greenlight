34b3ff33135950557f39eb073ee4a49e2238608a
Muhammad Naeem <mnaeem@sabbar.com>
2023-07-22 10:22:13 +0400

    added database connection pool config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 // Declare a string containing the application version number.
@@ -14,8 +18,14 @@ const version = "1.0.0"
 
 // Define a config struct to hold all the configuration settings for our application.
 type config struct {
-	port int    // network port that we want the server to listen on
-	env  string // the name of the current operating environment for the application (development, staging, production, etc.)
+	port int      // network port that we want the server to listen on
+	env  string   // the name of the current operating environment for the application (development, staging, production, etc.)
+	db   struct { // configuration settings for our database connection pool
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 }
 
 // Define an application struct to hold the dependencies for our HTTP handlers, helpers, and middleware.
@@ -32,10 +42,31 @@ func main() {
 	// default to using the port number 4000 and the environment "development" if no corresponding flags are provided.
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	//Read the DSN value from the db-dsn command-line flag into the config struct.
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
+	// Read the connection pool settings from command-line flags into the config struct.
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
 	flag.Parse()
 
 	// Initialize a new logger which writes messages to the standard out stream, prefixed with the current date and time.
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// Call the openDB() helper function to create the connection pool, passing in the config struct.
+	db, error := openDB(cfg)
+	if error != nil {
+		logger.Fatal(error)
+	}
+
+	// Defer a call to db.Close() so that the connection pool is closed before the main() function exits.
+	defer db.Close()
+
+	// Also log a message to say that the connection pool has been successfully established.
+	logger.Printf("database connection pool established")
 
 	// Declare an instance of the application struct, containing the config struct and the logger.
 	app := &application{
@@ -56,4 +87,42 @@ func main() {
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
 	err := srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+// The openDB() function returns a sql.DB connection pool.
+func openDB(cfg config) (*sql.DB, error) {
+	// Use sql.Open() to create an empty connection pool, using the DSN from the config // struct.
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the maximum number of open (in-use + idle) connections in the pool. Note that passing a value less than or equal to 0 will mean there is no limit.
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+
+	// Set the maximum number of idle connections in the pool.
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	// Use the time.ParseDuration() function to convert the idle timeout duration string to a time.Duration type.
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the maximum idle timeout.
+	db.SetConnMaxIdleTime(duration)
+
+	// Create a context with a 5-second timeout deadline.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Use PingContext() to establish a new connection to the database, passing in the context we created above as a parameter.
+	// If the connection couldn't be established successfully within the 5 second deadline, then this will return an error.
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return the sql.DB connection pool.
+	return db, nil
 }
