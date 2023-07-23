ca3ead32e103587485d0ad53fc6872f8340d0e28
Muhammad Naeem <mnaeem@sabbar.com>
2023-07-23 12:37:21 +0400

    added sorting in getAllMovies endpoint

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/lib/pq"
@@ -177,11 +178,13 @@ func (m MovieModel) Delete(id int64) error {
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
 	// Construct the SQL query to retrieve all movie records.
 	// Use full-text search for the title filter.
-	query := `
+	// Add an ORDER BY clause and interpolate the sort column and direction.
+	query := fmt.Sprintf(`
 			SELECT id, created_at, title, year, runtime, genres, version
 			FROM movies
-			WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') AND (genres @> $2 OR $2 = '{}')
-			ORDER BY id`
+			WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
+			AND (genres @> $2 OR $2 = '{}')
+			ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
 
 	// Create a context with a 3-second timeout.
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
