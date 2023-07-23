4ade6f87702f296b2acb73715b42058cbc56bfb0
Muhammad Naeem <mnaeem@sabbar.com>
2023-07-23 10:51:28 +0400

    added endpoint to get list of all movies

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -21,6 +21,8 @@ func (app *application) routes() *httprouter.Router {
 	// Register the relevant methods, URL patterns and handler functions for our endpoints using the HandlerFunc() method.
 	// http.MethodGet and http.MethodPost are constants which equate to the strings "GET" and "POST" respectively.
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
